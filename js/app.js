3df459a186eebd245f4ddaae44f8bf4da94bf29b
Jamie <jamiealden1@gmail.com>
2016-07-27 16:25:45 -0600

    Keydown Function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$(".ryu").mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
@@ -24,10 +24,23 @@ $(document).ready(function(){
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
 	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$(".ryu-still").hide();
+			$(".ryu-cool").show();
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}
+	});
 });
 
 function playHadouken () {
 	$("#hadouken-sound")[0].volume=0.5;
 	$("#hadouken-sound")[0].load();
 	$("#hadouken-sound")[0].play();
-}
+};
